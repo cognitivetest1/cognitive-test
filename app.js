d34a5759723a0fda3f5838ea1267d1e025b1fe56
cognitivetest1 <32683314+cognitivetest1@users.noreply.github.com>
2017-10-11 12:08:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 {
   "name": "Node.js Sample",
   "description": "A barebones Node.js app using Express 4 for email access",
-  "repository": "https://github.com/insurancecognitive1/emailaccess",
+  "repository": "https://github.com/cognitivetest1/cognitive-test",
   "keywords": ["node", "express", "static"]
 }
